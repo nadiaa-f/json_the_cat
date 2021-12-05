15400925fd99a392a2e05fe229aed246311c19ca
Nadia Fatah <nadi.fatah@gmail.com>
2021-12-05 10:16:55 +0000

    refactoring

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,23 +3,22 @@ const breedNotFound = "the mentioned breed not found in our database";
 
 const uriPath = "https://api.thecatapi.com/v1/breeds/search?q=";
 
-const args = process.argv;
-let breedInput = args[2];
-breedInput = uriPath + breedInput;
+const fetchBreedDescription = function(breedName, callback) {
 
-console.log(breedInput);
+
 
 request(breedInput, (error, response, body) => {
-if (error === null) {
-
-  console.log(`This is the Error code form API: ${error}`);
+if (error) {
+callback (`This is the Error code form API: ${error}`);
 }
 
 const data = JSON.parse(body);
   if (data[0] !== undefined) {
-  console.log(data[0]);
+  callback(null, data[0].description);
   } else {
-  console.log(breedNotFound);
+  callback(breedNotFound);
   }
 
 });
+};
+module.exports = {fetchBreedDescription}
